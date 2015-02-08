b715ef5f1bf61bfd27f2f505d5a1bc805d3200ae
tenshiPure <user.ryo@gmail.com>
2015-02-09 00:38:00 +0900

    fabric : create server setup task

--- fabfile.py
+++ fabfile.py
@@ -1,59 +1,110 @@
-import os.path
-from collections import namedtuple
-
-from fabric.api import env, run, put
+from fabric.api import env, run, sudo
 
 env.hosts = ['vagrant@127.0.0.1']
 env.port = '2200'
 env.key_filename = '~/.vagrant.d/insecure_private_key'
 
-current = os.path.abspath(os.path.dirname(__file__))
-configs = os.path.join(current, 'configs')
+import os
+devnull = open(os.devnull, 'w')
 
-# prepare
-def prepare():
-	run('rm /tmp/*')
-	default = 'hoge on\nfuga off\npiyo off'
-	run("echo '%s' > /tmp/put.conf" % default)
-	run("echo '%s' > /tmp/modify.conf" % default)
-	run("echo '%s' > /tmp/sed.conf" % default)
-	run("echo '%s' > /tmp/echo.conf" % default)
+def __yum():
+	sudo('yum update -y', stdout = devnull)
+	sudo('yum install -y yum-cron', stdout = devnull)
+	sudo('service yum-cron start')
+	sudo('chkconfig yum-cron on')
 
 
-# put
-def put_file():
-	put('%s/put.conf' % configs, '/tmp/put.conf')
+def __service():
+	services = [
+		'auditd',
+		'blk-availability',
+		'ip6tables',
+		'iscsi',
+		'iscsid',
+		'mdmonitor',
+		'netfs',
+		'nfslock',
+		'postfix',
+		'rpcbind',
+		'rpcgssd',
+		'rpcidmapd',
+		'udev-post'
+	]
+
+	[sudo('service %s stop' % service) for service in services]
+	[sudo('chkconfig %s off' % service) for service in services]
+
+	sudo('chkconfig --list | grep 3:on')
 
 
-# modify
-Pair = namedtuple('Pair', 'src dst')
+def __firewall():
+	ip = '10.0.2.15'
+	sudo('iptables -P INPUT ACCEPT')
+	sudo('iptables -P FORWARD ACCEPT')
+	sudo('iptables -P OUTPUT ACCEPT')
+	sudo('iptables -F INPUT')
+	sudo('iptables -F FORWARD')
+	sudo('iptables -F OUTPUT')
 
-def __modify(path, pairs):
-	for pair in pairs:
-		run('cp -p %s %s.tmp' % (path, path))
-		run("python -c \"print open('%s.tmp').read().replace('%s', '%s')\" > %s" % (path, pair.src, pair.dst, path))
-		run('rm %s.tmp' % path)
+	sudo('iptables -A INPUT -i lo -j ACCEPT')
+	sudo('iptables -A INPUT -p icmp -j ACCEPT')
+	sudo('iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT')
+	sudo('iptables -A INPUT -p tcp -d %(ip)s --dport 22 -j ACCEPT' % locals())
+	sudo('iptables -A INPUT -p tcp -d %(ip)s --dport 80 -j ACCEPT' % locals())
+	sudo('iptables -P INPUT DROP')
+	sudo('iptables -P FORWARD DROP')
+	sudo('iptables -P OUTPUT ACCEPT')
 
-def modify_file():
-	filepath = '/tmp/modify.conf'
-	pairs = [
-				Pair(src = 'fuga off', dst = 'fuga on'),
-				Pair(src = 'piyo off', dst = 'piyo on')
-			]
+	sudo('service iptables save')
+	sudo('service iptables restart')
 
-	__modify(filepath, pairs)
-
-
-# sed
-def sed_file():
-	run("sed -i.bak 's/fuga off/fuga on/' /tmp/sed.conf")
-	run("sed -i.bak 's/piyo off/piyo on/' /tmp/sed.conf")
-
-	# or
-	#run("sed -i.bak -e 's/fuga off/fuga on/' -e 's/piyo off/piyo on/' /tmp/sed.conf")
+	sudo('iptables --list')
 
 
-# echo
-def echo_file():
-	run("echo 'include append_echo.conf' >> /tmp/echo.conf")
-	put('%s/append_echo.conf' % configs, '/tmp/append_echo.conf')
+def diff(func):
+	def _diff(conf):
+		sudo('cp -p %s %s.bup' % (conf, conf))
+
+		func(conf)
+
+		sudo('diff %s %s.bup' % (conf, conf), warn_only = True)
+		sudo('rm %s.bup' % conf)
+
+	return _diff
+
+
+def __httpd_install():
+	sudo('yum install -y httpd', stdout = devnull)
+
+
+@diff
+def __httpd_conf(path):
+	sudo("sed -i 's/ServerTokens OS/ServerTokens Prod/' %s" % path)
+	sudo("sed -i 's/ScriptAlias \/cgi-bin\/ \"\/var\/www\/cgi-bin\/\"/#ScriptAlias/' %s" % path)
+	sudo("sed -i 's/Options Indexes FollowSymLinks/Options -Indexes FollowSymLinks/' %s" % path)
+	sudo("sed -i 's/ServerSignature On/ServerSignature Off/' %s" % path)
+
+
+def __httpd():
+	__httpd_install()
+	__httpd_conf('/etc/httpd/conf/httpd.conf')
+
+
+@diff
+def __sshd_conf(path):
+	sudo("sed -i 's/#Port 22/Port 22/' %s" % path) 
+	sudo("sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' %s" % path)
+	sudo("sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' %s" % path)
+	sudo("sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' %s" % path)
+
+
+def __sshd():
+	__sshd_conf('/etc/ssh/sshd_config')
+
+
+def vagrant():
+	__yum()
+	__service()
+	__firewall()
+	__httpd()
+	__sshd()
