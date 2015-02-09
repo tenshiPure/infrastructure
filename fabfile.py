fcb2b9c79229030216c2f21d7219e404b33ca18d
tenshiPure <user.ryo@gmail.com>
2015-02-09 22:55:59 +0900

    fabric : create deploy task

--- fabfile.py
+++ fabfile.py
@@ -113,3 +113,17 @@ def vagrant():
 	__firewall()
 	__httpd()
 	__sshd()
+
+
+def deploy():
+	noGit = run('rpm -q git', warn_only = True) == 'package git is not installed'
+
+	if noGit:
+		sudo('yum install -y git', stdout = devnull)
+
+	htmldir = '/var/www/html'
+	datetime = run('date +"%Y-%m-%d_%k-%M-%S"')
+
+	run('git clone https://github.com/tenshiPure/infrastructure.git %(htmldir)s/%(datetime)s' % locals(), stdout = devnull)
+	run('unlink %(htmldir)s/index.php' % locals())
+	run('ln -s %(htmldir)s/%(datetime)s/index.php %(htmldir)s/index.php' % locals())
--- setup.sh
+++ setup.sh
@@ -3,4 +3,9 @@ yum install -y httpd php
 echo "<?php echo date('Y-m-d');" > /var/www/html/sample.php
 ln -s /var/www/html/sample.php /var/www/html/index.php
 
+chown vagrant:vagrant /var/www/html/
+
+chown vagrant:vagrant /var/www/html/sample.php
+chown -h vagrant:vagrant /var/www/html/index.php
+
 service httpd start
