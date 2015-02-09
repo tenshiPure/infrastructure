2b394b68bd8e330d50ca80b13aa9ae4e3cc5aa70
tenshiPure <user.ryo@gmail.com>
2015-02-09 21:16:16 +0900

    fabric : modify index.php

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 yum install -y httpd php
 
-echo "<?php echo date('Y-m-d');" > /var/www/html/index.php
+echo "<?php echo date('Y-m-d');" > /var/www/html/sample.php
+ln -s /var/www/html/sample.php /var/www/html/index.php
 
 service httpd start
